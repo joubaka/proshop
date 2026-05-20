9b2ddac9731a86c1c87280ad730ef50e75f3dab2
pajoubert <hermanustennisacademy@gmail.com>
2026-05-20 09:01:47 +0200

    add batch_uuid column to activity_log table

---
+++ database/migrations/2024_01_01_000000_add_batch_uuid_to_activity_log_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBatchUuidToActivityLogTable extends Migration
+{
+    public function up()
+    {
+        Schema::table(config('activitylog.table_name'), function (Blueprint $table) {
+            if (!Schema::hasColumn(config('activitylog.table_name'), 'batch_uuid')) {
+                $table->uuid('batch_uuid')->nullable()->after('properties');
+            }
+        });
+    }
+
+    public function down()
+    {
+        Schema::table(config('activitylog.table_name'), function (Blueprint $table) {
+            $table->dropColumn('batch_uuid');
+        });
+    }
+}

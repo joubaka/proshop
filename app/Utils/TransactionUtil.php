0aa3dc35046956274d171f99415e1c679d84029b
pajoubert <hermanustennisacademy@gmail.com>
2026-06-18 11:58:51 +0200

    upload

--- app/Utils/TransactionUtil.php
+++ app/Utils/TransactionUtil.php
@@ -3574,7 +3574,7 @@ class TransactionUtil extends Util
         }
 
         $date = \Carbon::parse($transaction->transaction_date)
-                    ->addDays($edit_duration);
+                    ->addDays((int) $edit_duration);
 
         $today = today();
 
